7fa2eafc45a79811acc317125c5e4628cd999785
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-01-01 04:26:02 +0100

    unit tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,16 @@
 module Test.Main where
 
 import Prelude
+
 import Effect (Effect)
-import Effect.Console (log)
+import Main (myForeignFunction)
+import Test.Unit (test)
+import Test.Unit.Assert (assert)
+import Test.Unit.Main (runTest)
+import Test.Unit.QuickCheck (quickCheck)
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = runTest do
+    test "My tests" do
+        assert "f should double" $ myForeignFunction 5.0 == 10.0
+        quickCheck \x -> f x /= x -- quickCheck may not be a proper quick check
