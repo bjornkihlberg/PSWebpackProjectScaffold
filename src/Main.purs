649bb9103ab5a514a79a2ed8282a2a9ea75beb07
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-01-01 04:22:54 +0100

    setup ffi

--- src/Main.purs
+++ src/Main.purs
@@ -1,8 +1,11 @@
 module Main where
 
 import Prelude
+
 import Effect (Effect)
 import Effect.Console (log)
 
+foreign import myForeignFunction :: Number -> Number
+
 main :: Effect Unit
-main = log "Hello, world!"
+main = log $ show $ myForeignFunction 2.0
